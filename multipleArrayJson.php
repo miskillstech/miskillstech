be1b685e627db190d4c8434349400697d157a4ba
Agnikim <miskillstech@gmail.com>
2016-04-13 22:14:13 +0530

    test2 json file added for dave

--- multipleArrayJson.php
+++ multipleArrayJson.php
@@ -1,3 +1,3 @@
 <?php
-return '[{"id":"1", "name":"Dew Rawani", "email":"dave@gmail.com", "gender":"male", "nationality":"indian", "age":"22"}, {"id":"2", "name":"Agnikim Pro", "email":"agnikim88@gmail.com", "gender":"male", "nationality":"indian", "age":"25"}]';
+echo '[{"id":"1", "name":"Dew Rawani", "email":"dave@gmail.com", "gender":"male", "nationality":"indian", "age":"22"}, {"id":"2", "name":"Agnikim Pro", "email":"agnikim88@gmail.com", "gender":"male", "nationality":"indian", "age":"25"}]';
 ?>
--- singleArrayJson.php
+++ singleArrayJson.php
@@ -1,3 +1,3 @@
 <?php
-return '{"id":"1", "name":"Dew Rawani", "email":"dave@gmail.com", "gender":"male", "nationality":"indian", "age":"22"}';
+echo '{"id":"1", "name":"Dew Rawani", "email":"dave@gmail.com", "gender":"male", "nationality":"indian", "age":"22"}';
 ?>
