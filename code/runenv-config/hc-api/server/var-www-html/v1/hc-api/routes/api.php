b5d90499d0c4ff35a91b5f7c80eda52fdfc66b40
Agnikim <miskillstech@gmail.com>
2016-10-17 22:54:36 +0530

    updated hc api

--- code/runenv-config/hc-api/server/var-www-html/v1/hc-api/routes/api.php
+++ code/runenv-config/hc-api/server/var-www-html/v1/hc-api/routes/api.php
@@ -10,4 +10,6 @@ use Illuminate\Http\Request;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
-Route::get('suggestion','Search@fnGetSearchData');
+Route::get('testApi', function () {
+    echo 'Laravel is working';
+});
