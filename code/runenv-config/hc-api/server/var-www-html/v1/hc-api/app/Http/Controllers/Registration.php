e8f08491dc91e810a5dbaebe57e34d0cc6506d45
Agnikim <miskillstech@gmail.com>
2016-12-12 10:11:29 +0530

    updated hc api login reg

--- code/runenv-config/hc-api/server/var-www-html/v1/hc-api/app/Http/Controllers/Registration.php
+++ code/runenv-config/hc-api/server/var-www-html/v1/hc-api/app/Http/Controllers/Registration.php
@@ -48,16 +48,36 @@ class Registration extends Controller
 				)
 				->where('users_phone_verification_detail.uid', $prId)
 				->first();
-		if(!empty()) {
+		if(!empty($arPhoneData)) {
 			DB::table('users_phone_verification_detail')
 				->where('id', $arPhoneData->id)
-				->update(array('verificationCode' => $otp, 'isVerified' => 'no'))
+				->update(array('verificationCode' => $otp, 'isVerified' => 'no'));
 		} else {
 			DB::table('users_phone_verification_detail')->insert(
 				['uid' => $prId, 'verificationCode' => $otp]
 			);
 		}
 		return array('status' => 'success', 'otp' => $otp);
+	}
+	public function fnVerifyOTPForPhone() {
+		$requestData = Request::all();
+		$uid = $requestData['uid'];
+		$otp = $requestData['otp'];
+		$arPhoneData = DB::table('users_phone_verification_detail')
+				->select(
+					'users_phone_verification_detail.id'
+				)
+				->where('users_phone_verification_detail.uid', $uid)
+				->where('users_phone_verification_detail.verificationCode', $otp)
+				->first();
+		if(!empty($arPhoneData)) {
+			DB::table('users_phone_verification_detail')
+				->where('id', $arPhoneData->id)
+				->update(array('isVerified' => 'yes'));
+				return array('status' => 'success');
+		} else {
+			return array('status' => 'fail', 'message' => 'otp not matched! please try again.');
+		}
 	}
 	public function generateRandomString($length = 5) {
 		return substr(str_shuffle(str_repeat($x='0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ', ceil($length/strlen($x)) )),1,$length);
